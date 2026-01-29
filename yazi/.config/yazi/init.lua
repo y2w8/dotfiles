1974c1e951da298375cd49d7ac18d032b8d74cd9
y2w8 <yousefalharby416@gmail.com>
2026-01-29 14:14:26 +0300

    yazi: update

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,15 +1,66 @@
-require("relative-motions"):setup({
-	show_numbers = "relative",
-	show_motion = true,
-	only_motions = false,
-})
 require("duckdb"):setup({})
 require("sshfs"):setup()
 require("recycle-bin"):setup()
 require("folder-rules"):setup()
-require("starship"):setup()
+-- require("starship"):setup()
 -- require("omp"):setup({ config = "/home/y2w8/.config/ohmyposh/mocha.omp.json" })
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
+})
+local catppuccin_theme = require("yatline-catppuccin"):setup("mocha")
+require("yatline"):setup({
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "|", close = "|" },
+	inverse_separator = { open = "", close = "" },
+	show_background = false,
+  theme = catppuccin_theme,
+	header_line = {
+		left = {
+			section_a = {
+        			{type = "line", custom = false, name = "tabs", params = {"left"}},
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		},
+		right = {
+			section_a = {
+		      			-- {type = "string", custom = false, name = "date", params = {"%A, %d %B %Y"}},
+			},
+			section_b = {
+		      			-- {type = "string", custom = false, name = "date", params = {"%X"}},
+			},
+			section_c = {
+			}
+		}
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+        			{type = "string", custom = false, name = "tab_mode"},
+			},
+			section_b = {
+        			{type = "string", custom = false, name = "hovered_size"},
+			},
+			section_c = {
+        			{type = "string", custom = false, name = "hovered_path"},
+        			{type = "coloreds", custom = false, name = "count"},
+			}
+		},
+		right = {
+			section_a = {
+        			{type = "string", custom = false, name = "cursor_position"},
+			},
+			section_b = {
+        			{type = "string", custom = false, name = "cursor_percentage"},
+			},
+			section_c = {
+        			{type = "string", custom = false, name = "hovered_file_extension", params = {true}},
+        			{type = "coloreds", custom = false, name = "permissions"},
+			}
+		}
+	},
 })
