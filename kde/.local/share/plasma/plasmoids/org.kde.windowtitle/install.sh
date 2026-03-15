b8f966300199dc3381dccffb003e12006b121a09
y2w8 <yousefalharby416@gmail.com>
2026-03-15 21:24:11 +0300

    remove kde.

--- kde/.local/share/plasma/plasmoids/org.kde.windowtitle/install.sh
+++
@@ -1,5 +0,0 @@
-echo "Installing window-title-applet"
-rm -rf ~/.local/share/plasma/plasmoids/org.kde.windowtitle
-cp -r ./ ~/.local/share/plasma/plasmoids/org.kde.windowtitle
-systemctl --user restart plasma-plasmashell
-echo "Installed window-title-applet"
